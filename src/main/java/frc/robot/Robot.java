8832350e23dcb764bf01725f148a884ca52a9452
Joshua Tzeng <joshtzeng@gmail.com>
2020-01-07 19:49:02 -0500

    reversion back to the formatting mode code, trying to get vision value from network table didnt work. e Please enter the commit message for your changes. Lines starting

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -82,9 +82,7 @@ public class Robot extends TimedRobot {
  
   @Override
   public void autonomousPeriodic() {
-
-
-
+ 
   }
 
 
@@ -96,10 +94,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    m_Right1.set(j_Operator.getY());
-    m_RightElevator.set(-j_Operator.getY());
-    SmartDashboard.putNumber("Spark3", e_Right1.getVelocity());
-    SmartDashboard.putNumber("Spark6", e_RightElevator.getVelocity());
+
   }
 
 
