dbbe4703aa234f936c50a4498266ca4918007bac
Joshua Tzeng <joshtzeng@gmail.com>
2020-02-04 17:45:03 -0500

    Configured the ids of neos to work with comeptition bot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -39,12 +39,20 @@ public class Robot extends TimedRobot {
       public Joystick j_Operator = new Joystick(2);
 
     //neos
-      public CANSparkMax m_Left1 = new CANSparkMax(1, MotorType.kBrushless);
-      public CANSparkMax m_Left2 = new CANSparkMax(2, MotorType.kBrushless);
-      public CANSparkMax m_Right1 = new CANSparkMax(4, MotorType.kBrushless);
-      public CANSparkMax m_Right2 = new CANSparkMax(5, MotorType.kBrushless);
-      public CANSparkMax m_LeftElevator = new CANSparkMax(3, MotorType.kBrushless);
-      public CANSparkMax m_RightElevator = new CANSparkMax(6, MotorType.kBrushless);
+      public CANSparkMax m_Left1 = new CANSparkMax(12, MotorType.kBrushless);
+      public CANSparkMax m_Left2 = new CANSparkMax(13, MotorType.kBrushless);
+      public CANSparkMax m_Right1 = new CANSparkMax(1, MotorType.kBrushless);
+      public CANSparkMax m_Right2 = new CANSparkMax(2, MotorType.kBrushless);
+      public CANSparkMax m_Intake = new CANSparkMax(6, MotorType.kBrushless);
+      public CANSparkMax m_Feeder = new CANSparkMax(7, MotorType.kBrushless);
+      public CANSparkMax m_Tilting = new CANSparkMax(5, MotorType.kBrushless);
+      public CANSparkMax m_TopShooter = new CANSparkMax(10, MotorType.kBrushless);
+      public CANSparkMax m_BotShooter = new CANSparkMax(11, MotorType.kBrushless);
+      public CANSparkMax m_ControlPanel = new CANSparkMax(8, MotorType.kBrushless);
+      public CANSparkMax m_Climb = new CANSparkMax(3, MotorType.kBrushless);
+      public CANSparkMax m_LeftWinch = new CANSparkMax(9, MotorType.kBrushless);
+      public CANSparkMax m_RightWinch = new CANSparkMax(4, MotorType.kBrushless);
+
       public boolean inversion = true;
 
     //neo encoders
@@ -52,16 +60,34 @@ public class Robot extends TimedRobot {
       public CANEncoder e_Left2 = m_Left2.getEncoder();
       public CANEncoder e_Right1 = m_Right1.getEncoder();
       public CANEncoder e_Right2 = m_Right2.getEncoder();
-      public CANEncoder e_LeftElevator = m_LeftElevator.getEncoder();
-      public CANEncoder e_RightElevator = m_RightElevator.getEncoder();
+      public CANEncoder e_Intake = m_Intake.getEncoder();
+      public CANEncoder e_Feeder = m_Feeder.getEncoder();
+      public CANEncoder e_Tilting = m_Tilting.getEncoder();
+      public CANEncoder e_TopShooter = m_TopShooter.getEncoder();
+      public CANEncoder e_BotShooter = m_BotShooter.getEncoder();
+      public CANEncoder e_ControlPanel = m_ControlPanel.getEncoder();
+      public CANEncoder e_Climb = m_Climb.getEncoder();
+      public CANEncoder e_LeftWinch = m_LeftWinch.getEncoder();
+      public CANEncoder e_RightWinch = m_RightWinch.getPIDController();
+
+
 
     //neo pidcontrollers
-      public CANPIDController pc_Left1 = m_Left1.getPIDController();
-      public CANPIDController pc_Left2 = m_Left2.getPIDController();
-      public CANPIDController pc_Right1 = m_Right1.getPIDController();
-      public CANPIDController pc_Right2 = m_Right2.getPIDController();
-      public CANPIDController pc_LeftElevator = m_Left1.getPIDController();
-      public CANPIDController pc_RightElevator = m_Left1.getPIDController();
+      public CANEncoder e_Left1 = m_Left1.getPIDController();
+      public CANEncoder e_Left2 = m_Left2.getPIDController();
+      public CANEncoder e_Right1 = m_Right1.getPIDController();
+      public CANEncoder e_Right2 = m_Right2.getPIDController();
+      public CANEncoder e_Intake = m_Intake.getPIDController();
+      public CANEncoder e_Feeder = m_Feeder.getPIDController();
+      public CANEncoder e_Tilting = m_Tilting.getPIDController();
+      public CANEncoder e_TopShooter = m_TopShooter.getPIDController();
+      public CANEncoder e_BotShooter = m_BotShooter.getPIDController();
+      public CANEncoder e_ControlPanel = m_ControlPanel.getPIDController();
+      public CANEncoder e_Climb = m_Climb.getPIDController();
+      public CANEncoder e_LeftWinch = m_LeftWinch.getPIDController();
+      public CANEncoder e_RightWinch = m_RightWinch.getPIDController();
+
+
 
     //neo controllers
       public SpeedControllerGroup m_Left = new SpeedControllerGroup(m_Left1, m_Left2);
